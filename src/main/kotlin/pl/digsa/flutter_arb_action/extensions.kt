7c0d1aa5f9533fc7a3a11947ac1f0894120b46ca
psapekinfotower <psapek@infotower.pl>
2023-09-12 16:56:55 +0200

    Add simple implementation of replacing a string where BuildContext is available. Add fixed import to extension method.

--- src/main/kotlin/pl/digsa/flutter_arb_action/extensions.kt
+++ src/main/kotlin/pl/digsa/flutter_arb_action/extensions.kt
@@ -1,2 +1,6 @@
 package pl.digsa.flutter_arb_action
 
+inline fun <reified R> Sequence<*>.firstIsInstanceOrNull(): R? {
+    for (element in this) if (element is R) return element
+    return null
+}
