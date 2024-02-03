8c97db82d4efd1e7a8f9cdc5e76d418485f9252c
psapekinfotower <psapek@infotower.pl>
2024-02-03 23:10:18 +0100

    bump version

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -2,19 +2,17 @@
 
 object ModuleConfig {
     const val group = "pl.digsa"
-    const val version = "1.0.1"
+    const val version = "1.0.2"
 
     object IntellijPlugins {
         private object Versions {
             const val dart = "222.4582"
-            const val flutterIntl = "1.18.4-2022.2"
             const val yaml = "222.3739.67"
         }
 
         private const val _dart = "Dart:${Versions.dart}"
-        private const val _flutterIntl = "com.localizely.flutter-intl:${Versions.flutterIntl}"
         private const val _yaml = "org.jetbrains.plugins.yaml:${Versions.yaml}"
 
-        val plugins = listOf(_dart, _flutterIntl, _yaml)
+        val plugins = listOf(_dart, _yaml)
     }
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,12 +3,12 @@
 object Versions {
     const val java = "17"
     const val kotlin = "1.9.0"
-    const val intellij = "1.15.0"
+    const val intellij = "1.17.0"
 
     object Intellij {
         const val version = "2022.2.5"
         const val type = "IC"
         const val sinceBuild = "222"
-        const val untilBuild = "232.*"
+        const val untilBuild = "233.*"
     }
 }
