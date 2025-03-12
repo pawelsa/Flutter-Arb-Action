4fba90522bff71cb9fbf242c2de358d3f6ab6e4c
psapekinfotower <psapek@infotower.pl>
2025-03-13 00:33:34 +0100

    create HintTextField to show hints after text in TextFields

---
+++ src/main/kotlin/pl/digsa/flutter_arb_action/autohinting/HintTextField.kt
@@ -0,0 +1,46 @@
+package pl.digsa.flutter_arb_action.autohinting
+
+import com.intellij.ui.JBColor
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.RenderingHints
+import javax.swing.JTextField
+
+
+class HintTextField(columns: Int) : JTextField(columns) {
+    var hint: String? = null
+
+    override fun paintComponent(graphics: Graphics) {
+        super.paintComponent(graphics)
+
+        if (!hasFocus()) return
+        val hint = this.hint?.takeIf { it.isNotEmpty() } ?: return
+        val toPrint = getRestOfHintToShow(text, hint).takeIf { it.isNotEmpty() } ?: return
+
+        val graphics2D = (graphics.create() as Graphics2D).apply {
+            setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+            color = JBColor.GRAY
+            font = font
+        }
+        val endPosition = getFontMetrics(font).stringWidth(text)
+
+        graphics2D.drawString(
+            toPrint,
+            endPosition + 13,
+            21
+        )
+        graphics2D.dispose()
+    }
+}
+
+fun getRestOfHintToShow(enteredText: String, hint: String): String {
+    if (enteredText.isEmpty()) return hint
+
+    val parts = enteredText.split("(?=[A-Z])".toRegex())
+    val lastPart = parts.lastOrNull() ?: return hint
+
+    return when {
+        hint.startsWith(lastPart) -> hint.drop(lastPart.length)
+        else -> hint
+    }
+}
